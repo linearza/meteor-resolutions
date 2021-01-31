15fa80c88f27dd0bf38f1ad1c9c649a60349d45c
Michiel <linearza@users.noreply.github.com>
2021-01-31 15:45:42 +0200

    creates resolution component. adds user authentication

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,13 +1,38 @@
 <script>
   import { useTracker } from "meteor/rdb:svelte-meteor-data";
   import { Resolutions } from "../api/resolutions";
+  import { LoginWindow, Logout } from "meteor/levelup:svelte-accounts-ui";
+
+  import Resolution from "./resolutions/Resolution.svelte";
+  let newRes = "";
+
+  function handleSubmit(event) {
+    Resolutions.insert({
+      title: newRes,
+    });
+
+    newRes = "";
+  }
 
   $: resolutions = useTracker(() => Resolutions.find({}).fetch());
+  $: user = useTracker(() => Meteor.user());
 </script>
 
 <header>
   <h1>Yo! thats fresh</h1>
+
+  {#if $user && $user._id}
+    <Logout />
+  {:else}
+    <LoginWindow />
+  {/if}
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Add your resolution" bind:value={newRes} />
+    <button>Add</button>
+  </form>
+
   {#each $resolutions as resolution}
-    <h2>{resolution.title}</h2>
+    <Resolution {resolution} />
   {/each}
 </header>
